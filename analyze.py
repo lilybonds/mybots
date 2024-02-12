188bb8e4a2ce416067c3349e379f31c89f35926d
lilyb <lilyabonds@gmail.com>
2024-02-12 12:35:33 -0500

    finsihed sensors

--- analyze.py
+++ analyze.py
@@ -1,1 +1,11 @@
 import numpy
+import matplotlib.pyplot
+
+backLegSensorValues=numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues=numpy.load("data/frontLegSensorValues.npy")
+print(backLegSensorValues)
+print(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=2)
+matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2)
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
